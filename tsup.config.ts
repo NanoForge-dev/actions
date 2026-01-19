4648bbaa2bb491e13a129245edc86e19277f143b
Exelo <exelo.corp@gmail.com>
2026-01-19 17:01:14 +0100

    chore: add prepare and format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,35 +1,35 @@
 import { type Options, defineConfig } from "tsup";
 
 const createTsupConfig = ({
-                                   entry = ["src/index.ts"],
-                                   external = [],
-                                   noExternal = [],
-                                   platform = "node",
-                                   format = ["esm", "cjs"],
-                                   target = "es2022",
-                                   skipNodeModulesBundle = true,
-                                   clean = true,
-                                   shims = format.includes("cjs"),
-                                   cjsInterop = format.includes("cjs"),
-                                   minify = false,
-                                   terserOptions = {
-                                     mangle: false,
-                                     keep_classnames: true,
-                                     keep_fnames: true,
-                                   },
-                                   splitting = false,
-                                   keepNames = true,
-                                   dts = true,
-                                   sourcemap = true,
-                                   esbuildOptions = (options) => {
-                                     options.assetNames = "assets/[name]";
-                                   },
-                                   esbuildPlugins = [],
-                                   treeshake = false,
-                                   outDir = "dist",
-                                   publicDir = false,
-                                   loader = { ".wasm": "copy", ".yml": "copy" },
-                                 }: Options = {}) => {
+  entry = ["src/index.ts"],
+  external = [],
+  noExternal = [],
+  platform = "node",
+  format = ["esm", "cjs"],
+  target = "es2022",
+  skipNodeModulesBundle = true,
+  clean = true,
+  shims = format.includes("cjs"),
+  cjsInterop = format.includes("cjs"),
+  minify = false,
+  terserOptions = {
+    mangle: false,
+    keep_classnames: true,
+    keep_fnames: true,
+  },
+  splitting = false,
+  keepNames = true,
+  dts = true,
+  sourcemap = true,
+  esbuildOptions = (options) => {
+    options.assetNames = "assets/[name]";
+  },
+  esbuildPlugins = [],
+  treeshake = false,
+  outDir = "dist",
+  publicDir = false,
+  loader = { ".wasm": "copy", ".yml": "copy" },
+}: Options = {}) => {
   return defineConfig({
     entry,
     external,
@@ -54,7 +54,7 @@ const createTsupConfig = ({
     publicDir,
     loader,
   });
-}
+};
 
 const createConfig = (name: string, entries: string[] = []) => {
   return createTsupConfig({
