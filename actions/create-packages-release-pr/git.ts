8919daa624cea3a324347b58aa070bffbe123f91
MartinFILLON <martin.fillon@hotmail.com>
2026-06-13 19:15:34 +0200

    feat(git): add no verify to git add to

--- actions/create-packages-release-pr/git.ts
+++ actions/create-packages-release-pr/git.ts
@@ -5,7 +5,7 @@ export const checkoutToReleaseBranch = async (branchName: string): Promise<void>
 };
 
 export const commitAndPush = async (branchName: string, message: string): Promise<void> => {
-  await $`git add --all`;
+  await $`git add --all --no-verify`;
   await $`git -c user.name='github-actions[bot]' -c user.email='username@users.noreply.github.com' commit -m '${message}'`;
   await $`git push origin refs/heads/${branchName}:${branchName} --no-verify`;
 };
